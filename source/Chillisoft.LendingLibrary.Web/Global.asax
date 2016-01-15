7986a765b2f9c1b861888c98c0275ec478dbf61a
RyhilaEbrahim <Ryhila.Ebrahim@chillisoft.co.za>
2016-01-15 15:54:00 +0200

    Intial Commit

---
+++ source/Chillisoft.LendingLibrary.Web/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Chillisoft.LendingLibrary.Web.MvcApplication" Language="C#" %>
---
+++ source/Chillisoft.LendingLibrary.Web/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Chillisoft.LendingLibrary.Web.Startup))]
+namespace Chillisoft.LendingLibrary.Web
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
